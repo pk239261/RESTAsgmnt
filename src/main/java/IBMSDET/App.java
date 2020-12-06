acdbfa5b31bdfec296cb1c467718f23e3a0bee43
pk239261 <paras.khetwani@gmail.com>
2020-12-06 23:41:14 +0530

    RESTProjectCommit

---
+++ src/main/java/IBMSDET/App.java
@@ -0,0 +1,13 @@
+package IBMSDET;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
